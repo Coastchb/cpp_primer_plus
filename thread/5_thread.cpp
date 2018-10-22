d1372c8d152714bdb1e7a2feec7f96a4ec19625c
coast <Coastchb@sina.com>
2018-10-22 20:43:33 +0800

    gperftools added

--- thread/5_thread.cpp
+++ thread/5_thread.cpp
@@ -19,9 +19,9 @@ void do_task(long l, std::string& a, std::mutex& m);
 
 class Threads{
 public:
-    void run(int i, long n, std::string& aa, std::mutex& m){
+    void run(int i, long n, std::string& aa){
       for(int j=0;j < i; j++) {
-        thread_list_.push_back(std::thread(do_task, n, std::ref(aa), std::ref(m)));
+        thread_list_.push_back(std::thread(do_task, n, std::ref(aa), std::ref(mtx)));
       }
     }
     ~Threads(){
@@ -33,6 +33,7 @@ public:
     }
 
 private:
+    std::mutex mtx;
     std::vector<std::thread> thread_list_;
 };
 
@@ -42,31 +43,30 @@ int main(int argc, char *argv[]){
   std::string str;
   int a;
 
-  std::mutex mtx;
-
   // parallel mode
   time_t st2, ed2;
   st2 = time(NULL);
   {
     Threads tds;
-    tds.run(num_jobs, N, str, mtx);
+    tds.run(num_jobs, N, str);
   }
   ed2 = time(NULL);
   std::cout << "parallel2 mode consumed:" << (ed2 - st2) << "s" << std::endl;
 
-  std::ofstream fout("3_thread_out.txt");
+  std::ofstream fout("5_thread_out.txt");
   fout << str << std::endl;
 
-  do_task(10,str, mtx);
-  std::cout << str << std::endl;
+  //do_task(10,str, mtx);
+  //std::cout << str << std::endl;
 }
 
+void lock(std::mutex& m){
+  bool status = m.try_lock();
+  while(!status)
+    status = m.try_lock();
+}
 void do_task(long l, std::string& st, std::mutex& m){
-  for(long i = 0; i < l; i++){
-    if(m.try_lock()) {
-      st = st + std::to_string(l);
-      m.unlock();
-    }
-  }
-
+  lock(m);
+  st = st + std::to_string(l) + "\n";
+  m.unlock();
 }
