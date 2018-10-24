7a921a8088829345693edf0d6dbc29d857d675a0
coast <Coastchb@sina.com>
2018-10-24 19:55:15 +0800

    more added

--- gperftools/0.cpp
+++ gperftools/0.cpp
@@ -3,22 +3,33 @@
  */
 
 #include <gperftools/profiler.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string>
+using namespace std;
 
-void f()
+void f(int n )
 {
   int i;
-  for (i=0; i<1024*1024; ++i)
+  for (i=0; i<n*n; ++i)
   {
     char *p = (char*)malloc(1024*1024*120);
     free(p);
   }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+  if(argc != 2){
+    cout << "Usage: 0 num" << endl;
+    exit(1);
+  }
+
+  cout << argv[0] << endl;
+  cout << argv[1] << endl;
+  int num = atoi(argv[1]);
+  cout << num << endl;
   ProfilerStart("test.prof");//开启性能分析
-  f();
+  f(num);
   ProfilerStop();//停止性能分析
   return 0;
 }
@@ -29,5 +40,8 @@ int main()
 // g++ -ltcmalloc -lprofiler -o 0 0.cpp
 
 // run:
+// ./0 1024
+// and then
 // pprof --text ./0 test.prof >test.txt
+// or
 // pprof --pdf ./0 test.prof >test.pdf
---
+++ gperftools/1.cpp
@@ -0,0 +1,30 @@
+/**
+ * Created by coast on 2018/10/24
+ */
+#include <gperftools/profiler.h>
+#include <iostream>
+using namespace std;
+void func1() {
+  int i = 0;
+  while (i < 100000) {
+    ++i;
+  }
+}
+void func2() {
+  int i = 0;
+  while (i < 200000) {
+    ++i;
+  }
+}
+void func3() {
+  for (int i = 0; i < 1000; ++i) {
+    func1();
+    func2();
+  }
+}
+int main(){
+  ProfilerStart("my.prof"); // 指定所生成的profile文件名
+  func3();
+  ProfilerStop(); // 结束profiling
+  return 0;
+}
