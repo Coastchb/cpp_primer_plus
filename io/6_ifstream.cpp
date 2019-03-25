66958c04f8c124c8bfa9f3713aec802ec4e001de
coast <Coastchb@sina.com>
2019-03-25 11:09:17 +0800

    read file and parse line

---
+++ io/6_ifstream.cpp
@@ -0,0 +1,41 @@
+/**
+ * Created by coast on 2019/3/25
+ */
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+
+bool load_file(string& file, map<string,string>* dict) {
+  ifstream fis(file, ios_base::in);
+  if(fis.is_open()){
+    //string line;
+    char line[1024];
+    while(fis.getline(line, 1024)) {
+      size_t found = string(line).find(" ", 0);
+      if(found == string::npos){
+        continue;
+      }
+      string word = string(line).substr(0, found);
+      string pron = string(line).substr(found);
+      dict->emplace(word, pron);
+    }
+  }
+  return true;
+}
+
+int main(){
+  string file = "files/6.txt";
+  map<string,string> dict;
+  load_file(file, &dict);
+
+  for(auto& x: dict) {
+    cout << x.first << "\t" << x.second << endl;
+  }
+
+  cout << "pronunciation for 我 is: " << dict["我"] << endl;
+  return 0;
+
+}
