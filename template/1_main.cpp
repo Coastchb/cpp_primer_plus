d0a47171728cdf23704eca12c8913846b56bf403
coast <Coastchb@sina.com>
2019-04-20 19:31:59 +0800

    template

--- template/1_main.cpp
+++ template/1_main.cpp
@@ -4,11 +4,14 @@
 #include "1_template_class.h"
 
 int main(int argc, char* argv[]) {
-  Stack<int> stack_int;
+  Stack<int, 1> stack_int;
 
   stack_int.push(1);
   stack_int.push(2);
-  cout << stack_int.pop() << endl;
+  cout << "stack size:" << stack_int.get_size() << endl;
+  int ret;
+  stack_int.pop(ret);
+  cout << "poped:" << ret << endl;
   cout << "is empty?" << stack_int.is_empty() << endl;
 
   return 0;
--- template/1_template_class.h
+++ template/1_template_class.h
@@ -5,22 +5,71 @@
 #include <string>
 using namespace std;
 
+// 类模板成员函数实现必须要跟头文件在同一文件
+// reference:
+//   [1]http://www.cnblogs.com/lifexy/p/8781525.html
+//   [2]https://www.cnblogs.com/xiehongfeng100/p/4073867.html
+
 #ifndef _1_TEMPLATE_CLASS_
 #define _1_TEMPLATE_CLASS_
 
-template <typename T>
+template <typename T, int max_size>
 class Stack {
 private:
-    int num_; // element number;
+    int size_; // element number;
     T* list_;
+    int capacity_;
 
 public:
     Stack();
-    void push(T const&);
-    T const& pop();
-    bool is_empty() const;
+    bool push(T const&);
+    bool pop(T&);
+    bool is_empty() const {
+      return size_ == 0;
+    };
+    bool is_full() const {
+      return size_ == capacity_;
+    };
+    int get_size() const {
+      return size_;
+    }
     ~Stack();
 
 };
+
+template <typename T, int max_size>
+Stack<T, max_size>::Stack() {
+  size_ = 0;
+  capacity_ = max_size;
+  list_ = new T[capacity_];
+}
+
+template <typename T, int max_size>
+Stack<T, max_size>::~Stack() {
+  delete[] list_;
+}
+
+template <typename T, int max_size>
+bool Stack<T, max_size>::push(T const& e) {
+  if(is_full()) {
+    cout << "The stack is already full, refuse to push the element:" << e << "!\n";
+    return false;
+  }
+  list_[size_] = e;
+  size_++;
+  return true;
+}
+
+template <typename T, int max_size>
+bool Stack<T, max_size>::pop(T& e) {
+  if(is_empty()) {
+    cout << "The stack is empty, refuse to pop anything!\n";
+    return false;
+  }
+  size_--;
+  e = list_[size_];
+  return true;
+}
+
 
 #endif
